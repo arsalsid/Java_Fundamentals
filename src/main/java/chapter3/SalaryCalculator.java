64a6ed882d9d4714eaaa7fb28290e58f523ef4d2
Arsalan <arsalan.siddiqui@adroitlogix.co.uk>
2021-03-09 23:18:54 +0500

    Salary Calculator

---
+++ src/main/java/chapter3/SalaryCalculator.java
@@ -0,0 +1,28 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class SalaryCalculator {
+
+    /*
+    IF Statement.
+    All Salespeople get a payment of $1000 per week.
+    Salespeople who exceed a 10 sales get an additional bonus of $250.
+     */
+    public static void main(String[] args) {
+
+        int salary = 1000;
+        int bonus = 250;
+
+        System.out.println("How many sales did the employee make this week : ");
+
+        Scanner sc = new Scanner(System.in);
+        int sales = sc.nextInt();
+        sc.close();
+
+        if(sales > 10){
+            salary = salary + bonus;
+        }
+        System.out.println("The employee pay is $" +salary);
+    }
+}
