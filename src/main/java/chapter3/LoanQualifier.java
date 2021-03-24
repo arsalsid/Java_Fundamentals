0da521a5351fe915cdb41a6b12c933ce9464d373
Arsalan <arsalan.siddiqui@adroitlogix.co.uk>
2021-03-24 16:00:37 +0500

    LoanQualifier added

---
+++ src/main/java/chapter3/LoanQualifier.java
@@ -0,0 +1,42 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class LoanQualifier {
+    /* Nested IFS:
+        To qualify for a loan, a person must make at lease $30,000
+        and have been working at their working job at least 2 years.
+     */
+    public static void main(String[] args) {
+
+        int requiredSalary = 30000;
+        int requiredYearsEmployeed = 2;
+
+        System.out.println("Enter your Salary :" );
+        Scanner sc = new Scanner(System.in);
+        double salary = sc.nextDouble();
+
+
+        System.out.println("Enter the numbers of year with your current employer? " );
+        double years = sc.nextDouble();
+        sc.close();
+
+        if (salary >= requiredSalary){
+            if (years >= requiredYearsEmployeed){
+
+                System.out.println("Congrats! You qualify for the role...");
+            }
+            else {
+                System.out.println("Sorry! you must have worked in current employee of the job "
+                        + requiredYearsEmployeed  + " years.");
+            }
+        }
+            else {
+            System.out.println("Sorry! You must earn at least $"
+                    +requiredSalary + "to qualify for the loan...");
+        }
+
+
+
+    }
+}
