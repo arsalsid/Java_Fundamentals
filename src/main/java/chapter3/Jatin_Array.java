32baa98da3b036db3ae6d3e077bf092dc70e9dcf
Arsalan <arsalan.siddiqui@adroitlogix.co.uk>
2021-05-05 06:47:14 +0500

    Jatin_Array

--- src/main/java/chapter3/Jatin_Array.java
+++ src/main/java/chapter3/Jatin_Array.java
@@ -3,13 +3,14 @@ package chapter3;
 public class Jatin_Array {
     public static void main(String[] args) {
 
-/*
+    /*
         double itemArray [];
         itemArray = new double[5];
         int x [] = new int [3]; //Declaration of array, memory and size of array//
+    */
 
-*/
-        /* 1st type of write array
+    /*
+         1st type of write array
 
         int totalOfFiveItems [] = new int [5];//size of the array
         totalOfFiveItems[0] =32;
@@ -17,23 +18,19 @@ public class Jatin_Array {
         totalOfFiveItems[2] =321;
         totalOfFiveItems[3] =312;
         totalOfFiveItems[4] =312;
-     */
-        /*
+    */
+
+   /*
         System.out.println(totalOfFiveItems[0]); //index
         System.out.println(totalOfFiveItems[1]); //index
         System.out.println(totalOfFiveItems[2]); //index
-       */
+    */
 
         /* 2nd type of write array */
-        int totalOfFiveItems [] = {12 , 32 , 42 , 50 ,100};
+        int totalOfFiveItems[] = {12, 32, 42, 50, 100};
 
         System.out.println("Size of the Array" + totalOfFiveItems.length);
-        System.out.println(totalOfFiveItems [4]);
-
-
-
-
-
+        System.out.println(totalOfFiveItems[4]);
 
     }
 }
